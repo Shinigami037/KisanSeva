ecffa1b8469e8f74dd8bd3cbb36bf1b01dbb3784
Shinigami037 <ryderg4ming@gmail.com>
2022-07-06 13:28:39 +0530

    git ignore implemented

--- app/Views/pages/blog.php
+++ app/Views/pages/blog.php
@@ -39,7 +39,7 @@
 			<nav class="navbar navbar-expand-lg navbar-light main_box">
 				<div class="container">
 					<!-- Brand and toggle get grouped for better mobile display -->
-					<a class="navbar-brand logo_h" href="index"><img src="img/logo.png" alt=""></a>
+					<a class="navbar-brand logo_h" href=""><img src="img/logo.png" alt=""></a>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
 					 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="icon-bar"></span>
@@ -49,7 +49,7 @@
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
 						<ul class="nav navbar-nav menu_nav ml-auto">
-							<li class="nav-item"><a class="nav-link" href="index">Home</a></li>
+							<li class="nav-item"><a class="nav-link" href="">Home</a></li>
 							<li class="nav-item submenu dropdown">
 								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
 								 aria-expanded="false">Shop</a>
@@ -109,7 +109,7 @@
                 <div class="col-first">
                     <h1>Blog Page</h1>
                     <nav class="d-flex align-items-center">
-                        <a href="index">Home<span class="lnr lnr-arrow-right"></span></a>
+                        <a href="">Home<span class="lnr lnr-arrow-right"></span></a>
                         <a href="category">Blog</a>
                     </nav>
                 </div>
