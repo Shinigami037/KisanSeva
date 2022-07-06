012826892f1ed6cc4a0fc69aae1eba59f7b0c316
Shinigami037 <ryderg4ming@gmail.com>
2022-07-06 15:10:45 +0530

    Problem regarding css not working using php echo

--- app/Views/templates/default.php
+++ app/Views/templates/default.php
@@ -14,21 +14,21 @@
 	<meta name="keywords" content="">
 	<!-- meta character set -->
 	<meta charset="UTF-8">
-	<!-- Site Title -->
+	<!-- base Title -->
 	<title>Fit Health</title>
 	<!--
 		CSS
 		============================================= -->
-	<link rel="stylesheet" href="<?php echo site_url('css/linearicons.css');?>">
-	<link rel="stylesheet" href="<?php echo site_url('css/font-awesome.min.css');?>">
-	<link rel="stylesheet" href="<?php echo site_url('css/themify-icons.css');?>">
-	<link rel="stylesheet" href="<?php echo site_url('css/bootstrap.css');?>">
-	<link rel="stylesheet" href="<?php echo site_url('css/owl.carousel.css');?>">
-	<link rel="stylesheet" href="<?php echo site_url('css/nice-select.css');?>">
-	<link rel="stylesheet" href="<?php echo site_url('css/nouislider.min.css');?>">
-	<link rel="stylesheet" href="<?php echo site_url('css/ion.rangeSlider.css');?>"/>
-	<link rel="stylesheet" href="<?php echo site_url('css/font-awesome.min.css');?>"/>
-	<link rel="stylesheet" href="<?php echo site_url('css/main.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('css/linearicons.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('css/font-awesome.min.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('css/themify-icons.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('css/bootstrap.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('css/owl.carousel.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('css/nice-select.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('css/nouislider.min.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('css/ion.rangeSlider.css');?>"/>
+	<link rel="stylesheet" href="<?php echo base_url('css/font-awesome.min.css');?>"/>
+	<link rel="stylesheet" href="<?php echo base_url('css/main.css');?>">
 </head>
 
 <body>
@@ -50,21 +50,21 @@
 	<?php echo $this->include('templates/footer'); ?>
 	<!-- End footer Area -->
 
-	<script src="<?php echo site_url('js/vendor/jquery-2.2.4.min.js'); ?>"></script>
-	<script src="<?php echo site_url('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+	<script src="<?php echo base_url('js/vendor/jquery-2.2.4.min.js'); ?>"></script>
+	<script src="<?php echo base_url('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
 	 crossorigin="anonymous'); ?>"></script>
-	<script src="<?php echo site_url('js/vendor/bootstrap.min.js'); ?>"></script>
-	<script src="<?php echo site_url('js/jquery.ajaxchimp.min.js'); ?>"></script>
-	<script src="<?php echo site_url('js/jquery.nice-select.min.js'); ?>"></script>
-	<script src="<?php echo site_url('js/jquery.sticky.js'); ?>"></script>
-	<script src="<?php echo site_url('js/nouislider.min.js'); ?>"></script>
-	<script src="<?php echo site_url('js/countdown.js'); ?>"></script>
-	<script src="<?php echo site_url('js/jquery.magnific-popup.min.js'); ?>"></script>
-	<script src="<?php echo site_url('js/owl.carousel.min.js'); ?>"></script>
+	<script src="<?php echo base_url('js/vendor/bootstrap.min.js'); ?>"></script>
+	<script src="<?php echo base_url('js/jquery.ajaxchimp.min.js'); ?>"></script>
+	<script src="<?php echo base_url('js/jquery.nice-select.min.js'); ?>"></script>
+	<script src="<?php echo base_url('js/jquery.sticky.js'); ?>"></script>
+	<script src="<?php echo base_url('js/nouislider.min.js'); ?>"></script>
+	<script src="<?php echo base_url('js/countdown.js'); ?>"></script>
+	<script src="<?php echo base_url('js/jquery.magnific-popup.min.js'); ?>"></script>
+	<script src="<?php echo base_url('js/owl.carousel.min.js'); ?>"></script>
 	<!--gmaps Js-->
-	<script src="<?php echo site_url('https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE'); ?>"></script>
-	<script src="<?php echo site_url('js/gmaps.min.js'); ?>"></script>
-	<script src="<?php echo site_url('js/main.js'); ?>"></script>
+	<script src="<?php echo base_url('https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE'); ?>"></script>
+	<script src="<?php echo base_url('js/gmaps.min.js'); ?>"></script>
+	<script src="<?php echo base_url('js/main.js'); ?>"></script>
 </body>
 
 </html>
