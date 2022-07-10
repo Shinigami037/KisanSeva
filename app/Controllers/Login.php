065c09eb20f63c22ce233fcbc9669cab6c2f27ac
Shinigami037 <ryderg4ming@gmail.com>
2022-07-11 00:42:27 +0530

    Signup Data now getting added to the database and few bugs solved

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -17,9 +17,20 @@ class Login extends BaseController
     }
     public function userLogin()
     {
-        $newvar = var_dump($this->request->getVar("username"));
+        $validation = $this->validate([
+            'username' => 'required',
+            'password' => 'required|min_lenght[8]|max_lenght[16]',
+        ]);
+        // if(!$validation){
+        //     return view('user/login/registration',['validation'=>$this->validator]);
+        // }
+        // else{
+        //     // echo 'VValidated';
+        // }
+        $username = $this->request->getVar("username");
+        $password = $this->request->getVar("password");
         // echo "Hello $newvar";
-        var_dump(md5($this->request->getVar("password")));
+        // var_dump(md5($this->request->getVar("password")));
         return view('user/login/' . "login");
         // var_dump($this->request->getPost());
     }
@@ -32,7 +43,7 @@ class Login extends BaseController
         
         $values = [
             'user_name' => $username,
-            'user_password' => $password,
+            'user_password' => md5($password),
             'user_email' => $email,
             'customer_name' => $name,
         ];
@@ -44,6 +55,6 @@ class Login extends BaseController
         }else{
             return redirect()->to('registration')->with('Fail','Something went wrong');
         }
-        return view('user/login/' . "registration");
+        // return view('user/login/' . "registration");
     }
 }
